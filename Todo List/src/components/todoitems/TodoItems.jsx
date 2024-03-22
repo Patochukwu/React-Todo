e1146b5465e3c5078d3e782f27fd5ee9b711fa56
Patochukwu <christiantus22@gmail.com>
2024-03-22 18:53:37 +0100

    updated

--- Todo List/src/components/todoitems/TodoItems.jsx
+++ Todo List/src/components/todoitems/TodoItems.jsx
@@ -1,40 +1,15 @@
-// import { useState } from 'react'
-// import './TodoItems.css'
-
-// export const TodoItems = ({todoTask, deleteTask}) => {
-
-//   const [editing, setEditing] = useState(true)
-
-//   return (
-    
-//     <section className='task'>
-
-//         {!editing &&<p>{todoTask.task}</p>}
-//         {editing &&<input type="" value={todoTask.task} />}
-
-//         <section >
-//             <span className='span1' onClick={() => setEditing(prevVal => !prevVal)}>🖊️</span>
-            
-//             <span className='span2' onClick={() =>deleteTask(todoTask.id)}>🗑️</span>
-//         </section>
-
-//     </section>
-//   )
-// }
-
 import { useState } from 'react'
 import './TodoItems.css'
 
 export const TodoItems = ({ todoTask, deleteTask }) => {
 
-  const [editing, setEditing] = useState(false) // Changed to false initially
-
+  const [editing, setEditing] = useState(false) 
   return (
     
     <section className='task'>
 
         {!editing && <p>{todoTask.task}</p>}
-        {editing && <input type="text" value={todoTask.task} autoFocus />} {/* Added "text" type and autoFocus */}
+        {editing && <input type="text" value={todoTask.task} autoFocus />} 
 
         <section>
             <span className='span1' onClick={() => setEditing(prevVal => !prevVal)}>🖊️</span>
